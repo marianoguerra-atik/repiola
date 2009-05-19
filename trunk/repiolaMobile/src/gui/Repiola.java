77a111e204e9845ff36c903ac73540faebd5eaaf
luismarianoguerra <luismarianoguerra@52dc676a-4354-11de-ae54-e3549b14f2d9>
2009-05-19 03:13:22 +0000

    first import

---
+++ trunk/repiolaMobile/src/gui/Repiola.java
@@ -0,0 +1,265 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui;
+
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+import storage.Storage;
+
+/**
+ * @author mariano
+ */
+public class Repiola extends MIDlet implements CommandListener {
+    private boolean midletPaused = false;
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Fields ">//GEN-BEGIN:|fields|0|
+    private MobileCanvas mobileCanvas;
+    private TextBox textBox;
+    private Command backCommand;
+    private Command okCommand;
+    private Command backCommand1;
+    //</editor-fold>//GEN-END:|fields|0|
+
+    /**
+     * The Repiola constructor.
+     */
+    public Repiola() {
+        String text = Storage.load("code", 1);
+        
+        if(text != null)
+        {
+            getTextBox().setString(text);
+        }
+    }
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Methods ">//GEN-BEGIN:|methods|0|
+    //</editor-fold>//GEN-END:|methods|0|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: initialize ">//GEN-BEGIN:|0-initialize|0|0-preInitialize
+    /**
+     * Initilizes the application.
+     * It is called only once when the MIDlet is started. The method is called before the <code>startMIDlet</code> method.
+     */
+    private void initialize() {//GEN-END:|0-initialize|0|0-preInitialize
+        // write pre-initialize user code here
+//GEN-LINE:|0-initialize|1|0-postInitialize
+        // write post-initialize user code here
+    }//GEN-BEGIN:|0-initialize|2|
+    //</editor-fold>//GEN-END:|0-initialize|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: startMIDlet ">//GEN-BEGIN:|3-startMIDlet|0|3-preAction
+    /**
+     * Performs an action assigned to the Mobile Device - MIDlet Started point.
+     */
+    public void startMIDlet() {//GEN-END:|3-startMIDlet|0|3-preAction
+        // write pre-action user code here
+        switchDisplayable(null, getTextBox());//GEN-LINE:|3-startMIDlet|1|3-postAction
+        // write post-action user code here
+    }//GEN-BEGIN:|3-startMIDlet|2|
+    //</editor-fold>//GEN-END:|3-startMIDlet|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: resumeMIDlet ">//GEN-BEGIN:|4-resumeMIDlet|0|4-preAction
+    /**
+     * Performs an action assigned to the Mobile Device - MIDlet Resumed point.
+     */
+    public void resumeMIDlet() {//GEN-END:|4-resumeMIDlet|0|4-preAction
+        // write pre-action user code here
+//GEN-LINE:|4-resumeMIDlet|1|4-postAction
+        // write post-action user code here
+    }//GEN-BEGIN:|4-resumeMIDlet|2|
+    //</editor-fold>//GEN-END:|4-resumeMIDlet|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: switchDisplayable ">//GEN-BEGIN:|5-switchDisplayable|0|5-preSwitch
+    /**
+     * Switches a current displayable in a display. The <code>display</code> instance is taken from <code>getDisplay</code> method. This method is used by all actions in the design for switching displayable.
+     * @param alert the Alert which is temporarily set to the display; if <code>null</code>, then <code>nextDisplayable</code> is set immediately
+     * @param nextDisplayable the Displayable to be set
+     */
+    public void switchDisplayable(Alert alert, Displayable nextDisplayable) {//GEN-END:|5-switchDisplayable|0|5-preSwitch
+        // write pre-switch user code here
+        Display display = getDisplay();//GEN-BEGIN:|5-switchDisplayable|1|5-postSwitch
+        if (alert == null) {
+            display.setCurrent(nextDisplayable);
+        } else {
+            display.setCurrent(alert, nextDisplayable);
+        }//GEN-END:|5-switchDisplayable|1|5-postSwitch
+        // write post-switch user code here
+    }//GEN-BEGIN:|5-switchDisplayable|2|
+    //</editor-fold>//GEN-END:|5-switchDisplayable|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: mobileCanvas ">//GEN-BEGIN:|14-getter|0|14-preInit
+    /**
+     * Returns an initiliazed instance of mobileCanvas component.
+     * @return the initialized component instance
+     */
+    public MobileCanvas getMobileCanvas() {
+        if (mobileCanvas == null) {//GEN-END:|14-getter|0|14-preInit
+            // write pre-init user code here
+            mobileCanvas = new MobileCanvas();//GEN-BEGIN:|14-getter|1|14-postInit
+            mobileCanvas.setTitle("mobileCanvas");
+            mobileCanvas.addCommand(getBackCommand1());
+            mobileCanvas.setCommandListener(this);//GEN-END:|14-getter|1|14-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|14-getter|2|
+        return mobileCanvas;
+    }
+    //</editor-fold>//GEN-END:|14-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: commandAction for Displayables ">//GEN-BEGIN:|7-commandAction|0|7-preCommandAction
+    /**
+     * Called by a system to indicated that a command has been invoked on a particular displayable.
+     * @param command the Command that was invoked
+     * @param displayable the Displayable where the command was invoked
+     */
+    public void commandAction(Command command, Displayable displayable) {//GEN-END:|7-commandAction|0|7-preCommandAction
+        // write pre-action user code here
+        if (displayable == mobileCanvas) {//GEN-BEGIN:|7-commandAction|1|23-preAction
+            if (command == backCommand1) {//GEN-END:|7-commandAction|1|23-preAction
+                // write pre-action user code here
+                switchDisplayable(null, getTextBox());//GEN-LINE:|7-commandAction|2|23-postAction
+                // write post-action user code here
+            }//GEN-BEGIN:|7-commandAction|3|18-preAction
+        } else if (displayable == textBox) {
+            if (command == backCommand) {//GEN-END:|7-commandAction|3|18-preAction
+                // write pre-action user code here
+                exitMIDlet();//GEN-LINE:|7-commandAction|4|18-postAction
+                // write post-action user code here
+            } else if (command == okCommand) {//GEN-LINE:|7-commandAction|5|20-preAction
+                // write pre-action user code here
+                this.run();//GEN-LINE:|7-commandAction|6|20-postAction
+                // write post-action user code here
+            }//GEN-BEGIN:|7-commandAction|7|7-postCommandAction
+        }//GEN-END:|7-commandAction|7|7-postCommandAction
+        // write post-action user code here
+    }//GEN-BEGIN:|7-commandAction|8|
+    //</editor-fold>//GEN-END:|7-commandAction|8|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: textBox ">//GEN-BEGIN:|16-getter|0|16-preInit
+    /**
+     * Returns an initiliazed instance of textBox component.
+     * @return the initialized component instance
+     */
+    public TextBox getTextBox() {
+        if (textBox == null) {//GEN-END:|16-getter|0|16-preInit
+            // write pre-init user code here
+            textBox = new TextBox("textBox", ": begin\n\nrnd r0\nmod r0 100\nrnd r1\nmod r1 100\nrnd r2\nadd r3 1\nput r2\n\nlt r3 200 begin\n\n", 100, TextField.ANY);//GEN-BEGIN:|16-getter|1|16-postInit
+            textBox.addCommand(getBackCommand());
+            textBox.addCommand(getOkCommand());
+            textBox.setCommandListener(this);//GEN-END:|16-getter|1|16-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|16-getter|2|
+        return textBox;
+    }
+    //</editor-fold>//GEN-END:|16-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: backCommand ">//GEN-BEGIN:|17-getter|0|17-preInit
+    /**
+     * Returns an initiliazed instance of backCommand component.
+     * @return the initialized component instance
+     */
+    public Command getBackCommand() {
+        if (backCommand == null) {//GEN-END:|17-getter|0|17-preInit
+            // write pre-init user code here
+            backCommand = new Command("Back", Command.BACK, 0);//GEN-LINE:|17-getter|1|17-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|17-getter|2|
+        return backCommand;
+    }
+    //</editor-fold>//GEN-END:|17-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: okCommand ">//GEN-BEGIN:|19-getter|0|19-preInit
+    /**
+     * Returns an initiliazed instance of okCommand component.
+     * @return the initialized component instance
+     */
+    public Command getOkCommand() {
+        if (okCommand == null) {//GEN-END:|19-getter|0|19-preInit
+            // write pre-init user code here
+            okCommand = new Command("Ok", Command.OK, 0);//GEN-LINE:|19-getter|1|19-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|19-getter|2|
+        return okCommand;
+    }
+    //</editor-fold>//GEN-END:|19-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Getter: backCommand1 ">//GEN-BEGIN:|22-getter|0|22-preInit
+    /**
+     * Returns an initiliazed instance of backCommand1 component.
+     * @return the initialized component instance
+     */
+    public Command getBackCommand1() {
+        if (backCommand1 == null) {//GEN-END:|22-getter|0|22-preInit
+            // write pre-init user code here
+            backCommand1 = new Command("Back", Command.BACK, 0);//GEN-LINE:|22-getter|1|22-postInit
+            // write post-init user code here
+        }//GEN-BEGIN:|22-getter|2|
+        return backCommand1;
+    }
+    //</editor-fold>//GEN-END:|22-getter|2|
+
+    //<editor-fold defaultstate="collapsed" desc=" Generated Method: method ">//GEN-BEGIN:|32-entry|0|33-preAction
+    /**
+     * Performs an action assigned to the method entry-point.
+     */
+    public void method() {//GEN-END:|32-entry|0|33-preAction
+        // write pre-action user code here
+        this.run();//GEN-LINE:|32-entry|1|33-postAction
+        // write post-action user code here
+    }//GEN-BEGIN:|32-entry|2|
+    //</editor-fold>//GEN-END:|32-entry|2|
+
+    /**
+     * Returns a display instance.
+     * @return the display instance.
+     */
+    public Display getDisplay () {
+        return Display.getDisplay(this);
+    }
+
+    /**
+     * Exits MIDlet.
+     */
+    public void exitMIDlet() {
+        switchDisplayable (null, null);
+        destroyApp(true);
+        notifyDestroyed();
+    }
+
+    /**
+     * Called when MIDlet is started.
+     * Checks whether the MIDlet have been already started and initialize/starts or resumes the MIDlet.
+     */
+    public void startApp() {
+        if (midletPaused) {
+            resumeMIDlet ();
+        } else {
+            initialize ();
+            startMIDlet ();
+        }
+        midletPaused = false;
+    }
+
+    public void run()
+    {
+        Storage.save("code", textBox.getString(), 1);
+        getDisplay().setCurrent(mobileCanvas);
+        getMobileCanvas().setProgram(textBox.getString());
+    }
+    /**
+     * Called when MIDlet is paused.
+     */
+    public void pauseApp() {
+        midletPaused = true;
+    }
+
+    /**
+     * Called to signal the MIDlet to terminate.
+     * @param unconditional if true, then the MIDlet has to be unconditionally terminated and all resources has to be released.
+     */
+    public void destroyApp(boolean unconditional) {
+    }
+
+}
